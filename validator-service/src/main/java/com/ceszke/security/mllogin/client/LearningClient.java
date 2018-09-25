1d11fc6407e69eeb5f9f6d8ca7a3d0dfce1bef77
tomekceszke <tomek@ceszke.com>
2018-09-26 00:11:31 +0200

    dockerizing...

--- validator-service/src/main/java/com/ceszke/security/mllogin/client/LearningClient.java
+++ validator-service/src/main/java/com/ceszke/security/mllogin/client/LearningClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "learning-service", url = "${collector-service.ribbon.listOfServers:localhost}")
+@FeignClient(name = "learning-service", url = "${learning-service.ribbon.listOfServers:localhost}")
 public interface LearningClient {
 
     @GetMapping("/model")
